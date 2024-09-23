7779dd170448e40b11684ddd73d3ac5acf1c0e18
nagolove <dekar.own@gmail.com>
2024-09-23 15:16:16 +0300

    Comments

--- src/qtree_test.c
+++ src/qtree_test.c
@@ -1,31 +1,43 @@
 // vim: set colorcolumn=85
 // vim: fdm=marker
 
-#include "koh_rand.h"
-#include "koh_mm.h"
-#include "uthash.h"
+#include "koh_qtree.h"
 #include "munit.h"
 #include "stdbool.h"
 #include "stdbool.h"
-#include <unistd.h>
 #include <memory.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+
+/*
+Загрузить файл Quadtree.lua в Луа систему. Тестирование производить на
+сравнении с оригиналом. То ест вызывать методы оригинального кода.
+
+Будет задана некоторая последовальность точек и будет известен результат
+который я получаю от оригинального дерева.
+
+Вообще весь оригинальный результат я могу получить последовательность 
+записывая последовательность внутренний состояний из main.lua
+*/
 
 static bool verbose = true;
 
-static MunitResult test_new_add_exist_free(
+static MunitResult test_init_shutdown(
     const MunitParameter params[], void* data
 ) {
+    QTree q;
+    qtree_init(&q);
+    qtree_shutdown(&q);
 
     return MUNIT_OK;
 }
 
 static MunitTest test_suite_tests[] = {
   {
-    (char*) "/new_add_exist_free",
-    test_new_add_exist_free,
+    (char*) "/init_shutdown",
+    test_init_shutdown,
     NULL,
     NULL,
     MUNIT_TEST_OPTION_NONE,
@@ -36,12 +48,12 @@ static MunitTest test_suite_tests[] = {
 };
 
 static const MunitSuite test_suite = {
-  (char*) "strset",
- test_suite_tests,
- NULL,
- 1,
- MUNIT_SUITE_OPTION_NONE,
- .verbose = &verbose,
+    (char*) "quad tree",
+    test_suite_tests,
+    NULL,
+    1,
+    MUNIT_SUITE_OPTION_NONE,
+    .verbose = &verbose,
 };
 
 int main(int argc, char **argv) {
